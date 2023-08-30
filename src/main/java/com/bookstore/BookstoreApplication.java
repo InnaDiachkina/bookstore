e16b47a9944a475c5c7f880d285d27f3d81c775c
Inna Diachkina <innadyachkina@gmail.com>
2023-08-30 11:19:58 +0300

    my solution to the first part of the task

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -1,13 +1,37 @@
 package com.bookstore;
 
+import com.bookstore.model.Book;
+import com.bookstore.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookstoreApplication {
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(BookstoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Book book = new Book();
+                book.setPrice(BigDecimal.valueOf(123));
+                book.setAuthor("newAuthor");
+                book.setTitle("newBook");
+                book.setDescription("Good Book");
+                book.setIsbn("0123456789");
+                bookService.save(book);
+                System.out.println(bookService.findAll());
+            }
+        };
+    }
 }

311b8e7c06e352a8af70fd1e3d5981a0c490ac71
Inna Diachkina <innadyachkina@gmail.com>
2023-09-12 12:21:55 +0300

    used the correct annotations

--- src/main/java/com/bookstore/dto/request/CreateBookRequestDto.java
+++ src/main/java/com/bookstore/dto/request/CreateBookRequestDto.java
@@ -1,22 +1,20 @@
 package com.bookstore.dto.request;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
     @ISBN
     private String isbn;
-    @NotNull
-    @Min(value = 0)
+    @DecimalMin(value = "0.00")
     private BigDecimal price;
     private String description;
     private String coverImage;
